6cb9d5d22e92ba25b28d99f90e72e5a2d48716c2
shardhakoul <sk9225@nyu.edu>
2022-02-19 04:24:59 -0500

    My first commit

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-
+pip install django_on_heroku
 from pathlib import Path
 import django_on_heroku
 
