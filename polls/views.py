88e7a8a96b1e3ff515d0c4e50c8560f832a265c0
Shardha koul <85691194+Shardhakoul@users.noreply.github.com>
2022-03-04 22:24:33 -0500

    commit views

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question
-from django.urls import reverse
-from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
