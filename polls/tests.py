8349b854ad765ab7c8a1d5523e079c00b6bc64f9
Shardha koul <85691194+Shardhakoul@users.noreply.github.com>
2022-03-04 22:21:02 -0500

    commit test

--- polls/tests.py
+++ polls/tests.py
@@ -28,20 +28,6 @@ class QuestionIndexViewTests(TestCase):
             [question],
         )
 
-    def test_future_question(self):
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context["latest_question_list"], [])
-
-    def test_future_question_and_past_question(self):
-        question = create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(
-            response.context["latest_question_list"],
-            [question],
-        )
 
     def test_two_past_questions(self):
         question1 = create_question(question_text="Past question 1.", days=-30)
@@ -66,17 +52,6 @@ class QuestionDetailViewTests(TestCase):
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
-
-class QuestionModelTests(TestCase):
-    def test_was_published_recently_with_future_question(self):
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_old_question(self):
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
