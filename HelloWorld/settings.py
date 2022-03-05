bd58e9f892601f184edc89fe69c1f2657048b918
Shardha koul <85691194+Shardhakoul@users.noreply.github.com>
2022-03-04 21:47:14 -0500

    change-settings-1

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
+from dotenv import load_dotenv
+load_dotenv()
 
 import os
 from pathlib import Path
@@ -137,4 +139,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+django_heroku.settings(locals(), test_runner=False)
